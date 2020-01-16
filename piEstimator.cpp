124d170d65b1091b961c60a2942c7fe5d9f533bc
Aymane Boumaaza <aymaneboumaaza@gmail.com>
2020-01-16 20:52:57 +0100

    Update piEstimator.cpp
    Formatted the code

--- piEstimator.cpp
+++ piEstimator.cpp
@@ -10,24 +10,25 @@ using namespace std;
 default_random_engine gen;
 long double PI(int n){ 
     //initialising the number of points to zero
-    int in_circle = 0, in_square = 0;
+    int pts_in_circle = 0, pts_in_square = 0;
     uniform_real_distribution<double> uni_dist(0, 1);
     for (int i = 0; i < n; i++){
+        //generating points coordinates
         double x = uni_dist(gen);
         double y = uni_dist(gen);
         double d = sqrt((x * x) + (y * y));
         //calculating the number of points inside the circle (distance(point,center)<1)
-        if (d < 1)  in_circle++;
-        in_square++;
+        if (d < 1)  pts_in_circle++;
+        pts_in_square++;
     }
-    return 4 * (double) in_circle / in_square;
+    return 4 * (double) pts_in_circle / pts_in_square;
 }
 int main(){
     int n=1;
     while (n > 0){
-        cout << "number of points to generate = ";
+        cout << "Number of points to generate = ";
         cin >> n;
-        cout << PI(n)<< endl;
+        cout << "pi (approx.) = " << PI(n)<< endl;
     }
     return 0;
 }
